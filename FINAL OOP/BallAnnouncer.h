ff9830a40599a42fc18f1970c2e2f17ef9eb21a2
WitherenStar <moichi.muichirio@gmail.com>
2026-04-01 21:22:44 +0700

    Update BallAnnouncer.h
    It didn't send this one for some reason    - Ren

--- FINAL OOP/BallAnnouncer.h
+++ FINAL OOP/BallAnnouncer.h
@@ -1,36 +1,38 @@
-#pragma once
-#include <iostream>
-#include <vector>
-#include <random> //i used include algorithm, thinking i need it, but the code works fine without it so im just writing this just in case
-#include <string>
-using namespace std;
+#ifndef BALLANNOUNCER_H
+#define BALLANNOUNCER_H
 
+#include <vector>
+#include <string>
+
+using namespace std;
 
 class BallAnnouncer
 {
 private:
-	vector<int> numbers;
-	size_t index;
-	mt19937 rng; // Shuffles the numbers. As seen from: https://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution.html
-	char getLetter(int n);
-	bool hasNext();
+    vector<int>* ballPool;     //pointer to shuffled numbers 1–75
+    vector<int> drawnNumber;   //numbers already drawn
+    int currentNumber;
+    int currentIndex;
 
-	string callNext() {
-		if (!hasNext())
-			return "No more numbers.";
-
-		int n = numbers[index++]; //move onto the next index
-		char letter = getLetter(n); //get the letter corresponding the number they got
-		return string(1, letter) + "-" + to_string(n); //honestly its just there to make it look nicer, like B-13 and stuff
-	}
-	friend BallAnnouncer(); //https://www.geeksforgeeks.org/cpp/friend-class-function-cpp/  to access the private thing
 public:
-	//CONSTRUCTORS
-	BallAnnouncer();
+    int maxBalls;              //stops drawing at 35 / 45 / 55
 
+    //Difficulty levels (depending on how hard it is to get a bingo)
+    enum Difficulty
+    {
+        HARD = 35,
+        MEDIUM = 45,
+        EASY = 55
+    };
 
+    BallAnnouncer();                //constructor
+    ~BallAnnouncer();               //destructor
+
+    void setDifficulty();          //input + validation
+    void resetPool();              //create a vector from 1–75 and shuffle
+    int generateNumber();          //draw the next number
+    void displayNumber();          //show the B/I/N/G/O letter + number
+    void rollingAnimation();       //rolling animation
 };
 
- 
-};
-
+#endif
