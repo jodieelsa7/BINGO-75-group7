95fc464ddce1d85c22768f1899679da0b2aed829
jonathanalexx416-lab <jonathanalexx416@gmail.com>
2026-04-04 00:08:50 +0700

    updated changes for ballannouncer.cpp
    Added academic integrity declaration and comments.

--- FINAL OOP/BallAnnouncer.h
+++ FINAL OOP/BallAnnouncer.h
@@ -1,3 +1,15 @@
+/*
+Student Name: Gabriella Renata Virgie
+Student ID: 251103130115
+Course: FITS1201 – Object Oriented Programming
+Assignment: Final Project – Bingo-75 Game
+Academic Integrity Declaration:
+I declare that this work is my own and that I have not copied
+code from other students, websites, or online repositories
+without proper acknowledgement.
+*/
+
+
 #ifndef BALLANNOUNCER_H
 #define BALLANNOUNCER_H
 
@@ -32,7 +44,10 @@ public:
     void resetPool();              //create a vector from 1–75 and shuffle
     int generateNumber();          //draw the next number
     void displayNumber();          //show the B/I/N/G/O letter + number
+	void displayRecent();          //show the 3 recent drawn numbers
     void rollingAnimation();       //rolling animation
+	std::vector<int> getDrawnNumbers(); // to return the drawn numbers vector for saving/loading purposes
+	int getCurrentNumber();         // to return the current number for saving/loading purposes
 };
 
 #endif
