271b7ff4b841a4dcc116872f3a5c723cec0c25ef
jonathanalexx416-lab <jonathanalexx416@gmail.com>
2026-04-04 00:15:00 +0700

    Refactor Player class methods and constructors

--- FINAL OOP/Player.cpp
+++ FINAL OOP/Player.cpp
@@ -1,76 +1,81 @@
 #include "Player.h"
 #include <iostream>
 
-Player::Player() balance(100.0) {
+Player::Player() : balance(100.0) {
 	playerName = "TBA";
 	cards = nullptr;
 	numCards = 0;
+
 }
 
- void Player::setName(string name) {
-	 playerName = name;
+void Player::setName(string name) {
+	playerName = name;
+
 }
 
- void Player::setBalance(double newbalance) {
-	 balance = newbalance;
- }
+void Player::setBalance(double newBalance) {
+	balance = newBalance;
+}
 
- string Player::getName() const {
-	 return playerName;
- }
+string Player::getName() const {
+	return playerName;
+}
 
- double Player::getBalance() const { 
-	 return balance.getBalance();
- }
+Currency* Player::retBalance() {
+	return &balance;
+}
 
- void Player::addBalance(int amount) {
-	 balance.addWins(amount);
- }
+int Player::getNumCards() const {
+	return numCards;
+}
 
- void Player::deductBalance(int amount) {
-	 balance.subtractLoss(amount);
- }
+Cards Player::generateCard() {
+	cards = new Cards;
+	return *cards;
 
- int Player::getNumCards() const {
-	 return numCards;
- }
+}
 
- void Player::generateCard(int n) {
-	 delete[] cards;
-	 numCards = n;
-	 cards = new Card[numCards];
+void Player::displayCard() {
+	for (int i = 0; i < numCards; i++) {
+		cards[i].displayCards();
+	}
+}
 
- }
+void Player::markNumber(int number) {
+	for (int i = 0; i < numCards; i++) {
+		cards[i].markNumber(number);
+	}
+}
 
- void Player::displayCard() {
-	 for (int i = 0; i < numCards; i++) {
-		 cards[i].displayCard();
-	 }
- }
+bool Player::hasWon() {
+	for (int i = 0; i < numCards; i++) {
+		if (cards[i].checkPatterns()) {
+			return true;
+		}
+	}
+	return false;
+}
 
- void Player::markNumber(int number) {
-	 for (int i = 0; i < numCards; i++) {
-		 cards[i].markNumber(number);
-	 }
- }
+void Player::displayProfile() {
+	cout << "-------------------" << endl;
+	cout << "Name: " << playerName << endl;
+	cout << "Balance: " << balance.getBalance() << endl;
+	cout << "-------------------" << endl;
 
- bool Player::hasWon() {
-	 for (int i = 0; i < numCards; i++) {
-		 if (cards[i].hasWon()) {
-			 return true;
-		 }
-	 }
-	 return false;
- }
+}
 
- void Player::displayProfile() {
-	 cout << "-------------------" << endl;
-	 cout << "Name: " << playerName << endl;
-	 cout << "Balance: " << balance.getBalance() << endl;
-	 cout << "-------------------" << endl;
+void Player::loadPlayerCard(int loadedGrid[5][5], bool loadedMarked[5][5]) {
+	// Ensure the player has at least 1 card allocated before injecting data
+	if (numCards == 0) { generateCard(); }
+	cards[0].loadCardState(loadedGrid, loadedMarked);
+}
 
- }
+void Player::exportCardState(int outGrid[5][5], bool outMarked[5][5]) {
+	if (numCards > 0) {
+		cards[0].getCardState(outGrid, outMarked);
+	}
+}
 
- Player::~Player() {
-	 delete[] cards;
- }
+Player::~Player() {
+	delete[] cards;
+}
