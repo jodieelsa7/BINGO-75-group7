5d9eff94bec28fc798282f8d01ffd261339f9ccf
jonathanalexx416-lab <jonathanalexx416@gmail.com>
2026-04-04 15:00:58 +0700

    Refactor Player class card handling methods

--- FINAL OOP/Player.h
+++ FINAL OOP/Player.h
@@ -1,3 +1,27 @@
+/******************************************************************** 
+FITS1201 – Object-Oriented Programming 
+UNJI GAMES Assignment – Version 1 
+Academic Integrity Declaration 
+Student Name: Nazla Alifa Jannatin
+Student ID: 251103130125
+Submission Date: 4 April 2026
+I declare that: 
+1. This assignment is entirely my own original work. 
+2. I have not copied code from other students, websites, AI tools, 
+or any external sources without proper acknowledgment. 
+3. I have not used AI tools (such as ChatGPT, GitHub Copilot, or similar) 
+to generate any part of this assignment solution. 
+4. I have only used AI tools, if any, for learning purposes such as 
+understanding concepts, syntax, or debugging, and not for generating 
+code. 
+5. Any concepts, syntax, or techniques not taught in this course have been 
+properly acknowledged with citations in the comments of my code. 
+6. I understand that failure to comply with these requirements may result 
+in academic misconduct proceedings and penalties, including a mark of 
+zero for this assignment. 
+Student Signature: <Nazla Alifa Jannatin> 
+********************************************************************/
+
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <string>
@@ -29,7 +53,7 @@ public:
 	int getNumCards() const;
 
 	//Card functions
-	Cards generateCard();
+	void generateCard(int n);
 	void displayCard();
 	void markNumber(int number);
 
@@ -38,8 +62,8 @@ public:
 	void displayProfile();
 
 	//Importing and Exporting cards
-	void loadPlayerCard(int loadedGrid[5][5], bool loadedMarked[5][5]);
-	void exportCardState(int outGrid[5][5], bool outMarked[5][5]);
+	void loadPlayerCard(int index, int loadedGrid[5][5], bool loadedMarked[5][5]);
+	void exportCardState(int index, int outGrid[5][5], bool outMarked[5][5]);
 
 };
 
