a642bf6006ee53e3933529a0aea7adddd06134c0
jonathanalexx416-lab <jonathanalexx416@gmail.com>
2026-04-04 15:06:02 +0700

    Refactor Player class card handling methods

--- FINAL OOP/Player.cpp
+++ FINAL OOP/Player.cpp
@@ -1,3 +1,27 @@
+/******************************************************************** 
+FITS1201 – Object-Oriented Programming 
+UNJI GAMES Assignment – Version 1 
+Academic Integrity Declaration 
+Student Name: Nazla Alifa Jannatin
+Student ID: 251103130125
+Submission Date: 4 April 2026
+I declare that: 
+1. This assignment is entirely my own original work. 
+2. I have not copied code from other students, websites, AI tools, 
+or any external sources without proper acknowledgment. 
+3. I have not used AI tools (such as ChatGPT, GitHub Copilot, or similar) 
+to generate any part of this assignment solution. 
+4. I have only used AI tools, if any, for learning purposes such as 
+understanding concepts, syntax, or debugging, and not for generating 
+code. 
+5. Any concepts, syntax, or techniques not taught in this course have been 
+properly acknowledged with citations in the comments of my code. 
+6. I understand that failure to comply with these requirements may result 
+in academic misconduct proceedings and penalties, including a mark of 
+zero for this assignment. 
+Student Signature: <Nazla Alifa Jannatin> 
+********************************************************************/
+
 #include "Player.h"
 #include <iostream>
 
@@ -29,10 +53,10 @@ int Player::getNumCards() const {
 	return numCards;
 }
 
-Cards Player::generateCard() {
-	cards = new Cards;
-	return *cards;
-
+void Player::generateCard(int n) {
+	delete[] cards;              
+	numCards = n;                
+	cards = new Cards[numCards];
 }
 
 void Player::displayCard() {
@@ -64,15 +88,16 @@ void Player::displayProfile() {
 
 }
 
-void Player::loadPlayerCard(int loadedGrid[5][5], bool loadedMarked[5][5]) {
+void Player::loadPlayerCard(int index, int loadedGrid[5][5], bool loadedMarked[5][5]) {
 	// Ensure the player has at least 1 card allocated before injecting data
-	if (numCards == 0) { generateCard(); }
-	cards[0].loadCardState(loadedGrid, loadedMarked);
+	if (index < numCards) {
+		cards[index].loadCardState(loadedGrid, loadedMarked);
+	}
 }
 
-void Player::exportCardState(int outGrid[5][5], bool outMarked[5][5]) {
-	if (numCards > 0) {
-		cards[0].getCardState(outGrid, outMarked);
+void Player::exportCardState(int index, int outGrid[5][5], bool outMarked[5][5]) {
+	if (index < numCards) {
+		cards[index].getCardState(outGrid, outMarked);
 	}
 }
 
